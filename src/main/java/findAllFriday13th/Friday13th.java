03d46d24442b2c3b6f7b56deb612f7a5a7e8e7e9
fpavel93 <fpavel93@gmail.com>
2020-10-21 12:36:46 +0300

    friday 13 task

--- src/main/java/findAllFriday13th/Friday13th.java
+++ src/main/java/findAllFriday13th/Friday13th.java
@@ -4,28 +4,22 @@ package findAllFriday13th;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
-import java.util.Collections;
-import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.toMap;
-
 
 public class Friday13th {
-    public static Map<Integer, Long> findAllFriday13th(int start, int end) {
+    public static void findAllFriday13th(int start, int end) {
         LocalDate startDate = LocalDate.of(start, 1, 13);
-        LocalDate endDate = LocalDate.of(end,12,14);
-        return Stream.iterate(startDate,localDate -> localDate.plusMonths(1))
-                .limit(ChronoUnit.MONTHS.between(startDate,endDate))
-                .filter(localDate -> localDate.getDayOfWeek()==(DayOfWeek.FRIDAY))
-                .collect(Collectors.groupingBy(LocalDate::getYear,counting()))
+        LocalDate endDate = LocalDate.of(end, 12, 14);
+        Stream.iterate(startDate, localDate -> localDate.plusMonths(1))
+                .limit(ChronoUnit.MONTHS.between(startDate, endDate))
+                .filter(localDate -> localDate.getDayOfWeek() == (DayOfWeek.FRIDAY))
+                .collect(Collectors.groupingBy(LocalDate::getYear, counting()))
                 .entrySet().stream()
-                .sorted((o1, o2) -> (int) (o2.getValue()-o1.getValue()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+                .sorted((o1, o2) -> (int) (o2.getValue() - o1.getValue()))
+                .forEach(System.out::println);
     }
 }
--- src/main/java/findAllFriday13th/Main.java
+++ src/main/java/findAllFriday13th/Main.java
@@ -1,11 +1,7 @@
 package findAllFriday13th;
 
-import java.time.LocalDate;
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, Long> allFriday13th = Friday13th.findAllFriday13th(2000, 2020);
-        System.out.println(allFriday13th);
+        Friday13th.findAllFriday13th(2000, 2020);
     }
 }
