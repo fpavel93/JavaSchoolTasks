9d337c4fb8f8ad242a26efa4e5dad7d2197e8c59
fpavel93 <fpavel93@gmail.com>
2020-10-27 17:35:05 +0200

    Proxy Benchmark Annotation for method update

--- src/main/java/my_spring/iRobot/BenchmarkProxyConfigurator.java
+++ src/main/java/my_spring/iRobot/BenchmarkProxyConfigurator.java
@@ -1,49 +1,33 @@
 package my_spring.iRobot;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.Arrays;
 
 public class BenchmarkProxyConfigurator implements ProxyConfigurator {
     @Override
     public <T> Object wrapWithProxy(Object t, Class<T> type) {
-        if(type.isAnnotationPresent(Benchmark.class)){
+        if(type.isAnnotationPresent(Benchmark.class)|| Arrays.stream(type.getMethods()).anyMatch(method -> method.isAnnotationPresent(Benchmark.class))){
             return (T) Proxy.newProxyInstance(type.getClassLoader(), type.getInterfaces(), new InvocationHandler() {
                 @Override
                 public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                    System.out.println("*****BENCHMARK from method "+method.getName()+" started");
-                    long start = System.nanoTime();
-                    Object retVal = method.invoke(t, args);
-                    long end = System.nanoTime();
-                    System.out.println(end-start);
-                    System.out.println("*****BENCHMARK from method "+method.getName()+" finished");
-                    return retVal;
-                }
-            });
-        }
-        Method[] methods = type.getMethods();
-        for (Method method : methods) {
-            if(method.isAnnotationPresent(Benchmark.class)){
-                return (T) Proxy.newProxyInstance(type.getClassLoader(), type.getInterfaces(), new InvocationHandler() {
-                    @Override
-                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                        Object retVal;
-                        String name = method.getName();
-                        if(type.getMethod(name).isAnnotationPresent(Benchmark.class)) {
-                            System.out.println("*****BENCHMARK from method " + method.getName() + " started");
-                            long start = System.nanoTime();
-                            retVal = method.invoke(t, args);
-                            long end = System.nanoTime();
-                            System.out.println(end - start);
-                            System.out.println("*****BENCHMARK from method " + method.getName() + " finished");
-                        }
-                        else {
-                            retVal = method.invoke(t, args);
-                        }
+                    Method classMethod = type.getMethod(method.getName(), method.getParameterTypes());
+                    if(type.isAnnotationPresent(Benchmark.class)||classMethod.isAnnotationPresent(Benchmark.class)) {
+                        System.out.println("*****BENCHMARK from method " + method.getName() + " started");
+                        long start = System.nanoTime();
+                        Object retVal = method.invoke(t, args);
+                        long end = System.nanoTime();
+                        System.out.println(end - start);
+                        System.out.println("*****BENCHMARK from method " + method.getName() + " finished");
                         return retVal;
                     }
-                });
-            }
+                    else {
+                        return method.invoke(t,args);
+                    }
+                }
+            });
         }
         return t;
     }
